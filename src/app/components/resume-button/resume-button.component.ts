b373232329f1700676d8b272fbc2e5dafb4f6331
Anish George <anish.george@ignitesol.com>
2018-04-24 22:10:01 +0530

    Add back navigation in project details and refactor the floating container

--- src/app/components/resume-button/resume-button.component.ts
+++ src/app/components/resume-button/resume-button.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'resume-button',
-    template: `<a target="_blank" href="{{link}}">Download Resume</a>`,
-    styleUrls: ['./resume-button.component.styl']
+    template: `<a target="_blank" class="basic-button" href="{{link}}">Download Resume</a>`
 })
 export class ResumeButtonComponent {
     link = require("docs/Shivani Arvikar_UI-UX Designer_Resume.pdf")
