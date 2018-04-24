6c86450d56e76389e86bd6e37018bc37a4d08d25
Anish George <anish.george@ignitesol.com>
2018-04-24 22:27:02 +0530

    Add some shadows!!

--- src/app/components/resume-button/resume-button.component.ts
+++ src/app/components/resume-button/resume-button.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'resume-button',
-    template: `<a target="_blank" class="basic-button" href="{{link}}">Download Resume</a>`
+    template: `<a target="_blank" [class.depth-2]="floating" class="basic-button" href="{{link}}">Download Resume</a>`
 })
 export class ResumeButtonComponent {
     link = require("docs/Shivani Arvikar_UI-UX Designer_Resume.pdf")
+    @Input() floating: boolean;
 }
