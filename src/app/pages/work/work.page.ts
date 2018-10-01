73a3bd758c8c71ef7652fb31da9b308fff7529d4
Anish George <anishgeo@thoughtworks.com>
2018-10-02 00:20:57 +0530

    Fix the masonry problem

--- src/app/pages/work/work.page.ts
+++ src/app/pages/work/work.page.ts
@@ -1,10 +1,23 @@
-import { Component, HostBinding } from '@angular/core';
+import {Component, HostBinding, OnDestroy, Renderer2} from '@angular/core';
 
 @Component({
     selector: 'work-page',
     styleUrls: ['./work.page.styl', '../page.styl'],
     templateUrl: './work.page.html'
 })
-export class WorkPage {
+export class WorkPage implements OnDestroy {
     @HostBinding('class.off-white-page') offWhitePage = true;
+
+
+    constructor(
+        private renderer: Renderer2
+    ) {
+        this.renderer.addClass(document.body, 'off-white-page');
+        this.renderer.addClass(document.body, 'padded-page');
+    }
+
+    ngOnDestroy() {
+        this.renderer.removeClass(document.body, 'off-white-pagee');
+        this.renderer.removeClass(document.body, 'padded-page');
+    }
 }
